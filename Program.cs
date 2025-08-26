d5bc2170a697285c128a5bb622873cb5586cb97d
Riley <rly907@gmail.com>
2025-08-26 14:34:50 -0600

    Removed redundant files, started adding features

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Terminal.Gui;
+using Terminal.Gui.Configuration;
+using Terminal.Gui.App;
+using Terminal.Gui.Drawing;
+using Terminal.Gui.ViewBase;
+using Terminal.Gui.Views;
+
+Application.Run<ExampleWindow> ().Dispose ();
+Application.Shutdown ();
+
+public class ExampleWindow : Window {
+    public ExampleWindow(){
+        Title = $"Example ({Application.QuitKey} To Exit)";
+
+        var InputCode = new TextField
+        {
+            X = 2,Y = 1,
+
+            // Fill remaining horizontal space
+            Width = 10
+        };
+    Add (InputCode);
+    }
+
+}
